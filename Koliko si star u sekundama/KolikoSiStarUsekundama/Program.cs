5fd39c4e3a9a201e7fbab47e054b38841bfb2ac0
Dusan Kulasinovic <kulasinovicdusan@gmail.com>
2020-12-15 19:48:31 +0100

    Koliko si star u sekundama
    Aplikacija trazi da unesete dan, mesec i godinu rodjenja nakon cega ispisuje starost u sekundama. Zasnovano na lokalnom vremenu vaseg racunara.

---
+++ Koliko si star u sekundama/KolikoSiStarUsekundama/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace KolikoSiStarUsekundama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite dan, mesec i godinu rodjenja");
+
+            Console.WriteLine("Dan rodjenja?");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mesec rodjenja?");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Godina rodjenja?");
+            int year = Convert.ToInt32(Console.ReadLine());
+            
+            
+            DateTime godinaRodjenja = new DateTime(year, month, day);
+            DateTime trenutnoVreme = DateTime.Now;
+            TimeSpan ts = trenutnoVreme - godinaRodjenja;
+            Console.WriteLine("Stari ste tacno " + ts.TotalSeconds + " sekundi");
+
+        }
+    }
+}
