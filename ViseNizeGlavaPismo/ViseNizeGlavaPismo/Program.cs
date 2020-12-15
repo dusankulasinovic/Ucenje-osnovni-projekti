d6ce94230d55653f1e3e7baf59448a203f6166c4
Dusan Kulasinovic <kulasinovicdusan@gmail.com>
2020-12-15 19:46:55 +0100

    Vise nize glava pismo
    Konzolna aplikacija koja vam daje opciju da igrate igru vise nize gde pogadjate broj od 1 do 100 kao i igru glava pismo gde kompjuter nasumicno bira glavu ili pismo.

---
+++ ViseNizeGlavaPismo/ViseNizeGlavaPismo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ViseNizeGlavaPismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Da li zelite da igrate igru Vise-Nize ili Glave-Pismo\n"+ "1.Vise-Nize\n" + "2. Glava-pismo\n");
+            int odabirIgre = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //Vise nize
+            if (odabirIgre == 1)
+            {
+                Random rnd = new Random();
+                int zadatiBroj = rnd.Next(1, 100);
+                int pokusaniBroj = 0;
+                Console.WriteLine("Pogodite broj od 1 do 100");
+                while (pokusaniBroj != zadatiBroj)
+                {
+                    pokusaniBroj = Convert.ToInt32(Console.ReadLine());
+
+                    if (pokusaniBroj < zadatiBroj)
+                    {
+                        Console.WriteLine("Vise\n");
+                    }
+
+                    else if (pokusaniBroj > zadatiBroj)
+                    {
+                        Console.Write("Nize\n");
+                    }
+                }
+                Console.WriteLine("Pogodili ste trazeni broj " + zadatiBroj);
+                Console.Read();
+
+            }
+
+            //Glava Pismo
+            else if (odabirIgre == 2)
+            {
+                while (true) {
+                
+                    Random rnd = new Random();
+                    List<string> glavaPismo = new List<string> { "Glava", "Pismo" };
+                    var random = new Random();
+                    int indexGlavaPismo = random.Next(glavaPismo.Count);
+                    Console.WriteLine(glavaPismo[indexGlavaPismo] + "\n Pritisnite enter za ponovni pokusaj");
+                    Console.ReadLine();
+                }
+
+            }
+            
+        }
+    }
+}
